45c80dfe2ac7a5eb830fbfa45e60df7ebb4fbbb3
Shadow0902 <76466292+Shadow0902@users.noreply.github.com>
2021-04-27 15:48:17 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://shadow0902.github.io/image-ai/';
+let imageModelURL = 'https://shadow0902.github.io/ImageAi/';
 
 // Video
 let video;
